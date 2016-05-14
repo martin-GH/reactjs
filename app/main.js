c56b6d5cb75e34fb32b4d34a4cd193eb53af1d5d
Martin <mk@zwommel.de>
2016-05-14 18:09:22 +0200

    setup

---
+++ app/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HeaderComponent from './components/HeaderComponent';
+
+ReactDOM.render(<HeaderComponent />, document.getElementById('app'));
